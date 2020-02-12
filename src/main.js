fc5e9121bf19e0235022bfd179e4579940c9f026
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-02-12 17:20:58 +0800

    新增Layout與解決a-menu-item中添加router-link的CSS問題

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router" //引入 router
+import 'ant-design-vue/dist/antd.css';
 
 Vue.config.productionTip = false
 
+import { Button } from 'ant-design-vue';
+Vue.use(Button);
+import { Menu } from 'ant-design-vue';
+Vue.use(Menu);
+import { Icon } from 'ant-design-vue';
+Vue.use(Icon);
+import { Layout } from 'ant-design-vue';
+Vue.use(Layout);
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router
 }).$mount('#app')
---
+++ src/page/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  </div>
+</template>
