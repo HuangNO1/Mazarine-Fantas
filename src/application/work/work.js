f1939df424c03d438a938605d5db2e5d4ac1fbcc
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-02-13 16:47:31 +0800

    裝回ant-design並解決預設頁面載入

--- src/application/work/work.js
+++ src/application/work/work.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './work.vue'
 import router from "./router" //引入 router
-import 'bootstrap/dist/js/bootstrap.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'ant-design-vue/dist/antd.css';
+
+import { Button } from 'ant-design-vue';
+Vue.use(Button);
+import { Icon } from 'ant-design-vue';
+Vue.use(Icon);
+import { Layout } from 'ant-design-vue';
+Vue.use(Layout);
+import { Menu } from 'ant-design-vue';
+Vue.use(Menu);
+import { Breadcrumb } from 'ant-design-vue';
+Vue.use(Breadcrumb);
 
 Vue.config.productionTip = false
 
